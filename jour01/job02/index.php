7df42d0c03c77a173e5510ca06e1f387ebb26dac
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-07-21 12:20:36 +0200

    Job02 : affichage Hello LaPlateforme et commentaires

---
+++ jour01/job02/index.php
@@ -0,0 +1,11 @@
+<?php
+//affiche "Hello la Plateforme!" première fois
+echo "Hello la Plateforme!<br>";
+
+/*
+Affiche "Hello la Plateforme!" une deuxième fois
+Dans cette partie le commentaire s'affiche en bloc
+*/
+echo "Hello la Plateforme!<br>";
+
+?>
