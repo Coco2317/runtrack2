45c2d2c854b9f23f60f530abec29ccf4feac48cf
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-07-22 12:57:16 +0200

    job07 : affichage triangle avec boucle imbriquée

---
+++ jour02/job07/index.php
@@ -0,0 +1,16 @@
+<?php
+$hauteur = 5;
+for ($i = 1; $i <= $hauteur; $i++){
+    for ($j = 1; $j <= $hauteur - $i; $j++){
+echo "  ";
+    }
+    for ($k = 1; $k <= 2 * $i-1; $k++){
+        echo"*";
+    }
+    echo"<br>";
+}
+?>
+
+
+
+
