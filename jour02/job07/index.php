dae678cc8692d765ea5069f2e4ccf633426a49ae
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-07-23 09:35:55 +0200

    Mise à jour code triangle isocèle avec balise <pre>

--- jour02/job07/index.php
+++ jour02/job07/index.php
@@ -1,16 +1,21 @@
 <?php
+echo "<pre>"; 
+
 $hauteur = 5;
-for ($i = 1; $i <= $hauteur; $i++){
-    for ($j = 1; $j <= $hauteur - $i; $j++){
-echo "  ";
+
+for ($i = 1; $i <= $hauteur; $i++) {
+    
+    for ($j = 1; $j <= $hauteur - $i; $j++) {
+        echo " ";
     }
-    for ($k = 1; $k <= 2 * $i-1; $k++){
-        echo"*";
+
+    
+    for ($k = 1; $k <= 2 * $i - 1; $k++) {
+        echo "*";
     }
-    echo"<br>";
+
+    echo "\n"; 
 }
+
+echo "</pre>"; 
 ?>
-
-
-
-
