0c8964777e58b60eb83ec99c67da5f0c12eacbbb
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-07-23 12:05:18 +0200

    Créez un algorithme qui parcourt, compte et affiche le nombre de caractères présents dans cette chaîne.

---
+++ jour03/job04/index.php
@@ -0,0 +1,10 @@
+<?php
+$str = "Dans l'espace, personne ne vous entend crier";
+$compteur = 0;
+
+for ($i = 0; $i < strlen($str); $i++) {
+    $compteur++;
+}
+echo "La chaîne contient $compteur caractères.";
+
+?>
