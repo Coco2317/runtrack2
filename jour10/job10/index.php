88a299b41fa0f47bff053176c969d869f42f5a52
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-08-04 10:07:34 +0200

    job10 : salles par capacite croissante

---
+++ jour10/job10/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Salles par capacité croissante</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1> Liste des salles (capacité croissante)</h1>
+
+<?php
+
+$connexion = mysqli_connect("localhost", "root", "", "jour09");
+
+if (!$connexion) {
+    die("<div class='message'>Erreur de connexion : " . mysqli_connect_error() . "</div>");
+}
+
+$requete = "SELECT * FROM salles ORDER BY capacite ASC";
+$resultat = mysqli_query($connexion, $requete);
+
+if ($resultat && mysqli_num_rows($resultat) > 0) {
+    echo "<table>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>nom</th>
+                    <th>id_etage</th>
+                    <th>capacite</th>
+                </tr>
+            </thead>
+            <tbody>";
+
+    while ($ligne = mysqli_fetch_assoc($resultat)) {
+        echo "<tr>
+                <td>" . htmlspecialchars($ligne['id']) . "</td>
+                <td>" . htmlspecialchars($ligne['nom']) . "</td>
+                <td>" . htmlspecialchars($ligne['id_etage']) . "</td>
+                <td>" . htmlspecialchars($ligne['capacite']) . "</td>
+              </tr>";
+    }
+
+    echo "</tbody></table>";
+} else {
+    echo "<div class='message'>Aucune salle trouvée.</div>";
+}
+
+mysqli_close($connexion);
+?>
+
+</body>
+</html>
